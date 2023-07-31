26ee2f8d0538b0fccb2cae45326dc0f6b14a2ec0
brnydr <brianwilliamyoder@gmail.com>
2023-07-31 16:36:22 -0700

    fix

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,25 +1,18 @@
-export default class WeatherService {
-
- static getWeather(city) {
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-    request.addEventListener("loadend", function() {
-      const response = JSON.parse(this.responseText);
-      if (this.status === 200) {
-        resolve([response, city]);
-      } else {
-        reject([this, response, city]);
-      }
-    });
-    request.open("GET", url, true);
-    request.send();
-  });
-
-    promise.then(function(weatherDataArray) {
-      printElements(weatherDataArray);
-    }, function(errorArray) {
-      printError(errorArray);
+export default class WeatherService {  
+  static getWeather(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, city]);
+        } else {
+          reject([this, response, city]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
     });
   }
 }
